d371e9012bbc5a1824379c1f06717d1e128db3f6
James Oliver <joliver@rancher.com>
2018-04-02 16:46:22 -0700

    Implement instance create/delete endpoints

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -6,21 +6,25 @@ import (
 
   "github.com/golang/glog"
   "github.com/gorilla/mux"
+  metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
   "k8s.io/apimachinery/pkg/labels"
   "k8s.io/client-go/tools/cache"
 
   vmapi "github.com/llparse/kube-crd-skel/pkg/apis/ranchervm/v1alpha1"
+  vmclientset "github.com/llparse/kube-crd-skel/pkg/client/clientset/versioned"
   vminformers "github.com/llparse/kube-crd-skel/pkg/client/informers/externalversions/virtualmachine/v1alpha1"
   vmlisters "github.com/llparse/kube-crd-skel/pkg/client/listers/virtualmachine/v1alpha1"
 )
 
 type server struct {
+  vmClient   vmclientset.Interface
   vmLister        vmlisters.VirtualMachineLister
   vmListerSynced  cache.InformerSynced
 }
 
-func NewServer(vmInformer vminformers.VirtualMachineInformer) *server {
+func NewServer(vmClient vmclientset.Interface, vmInformer vminformers.VirtualMachineInformer) *server {
   return &server{
+    vmClient: vmClient,
     vmLister: vmInformer.Lister(),
     vmListerSynced: vmInformer.Informer().HasSynced,
   }
@@ -45,7 +49,22 @@ func (s *server) newRouter() *mux.Router {
   // r.Methods("GET").Path("/").Handler(versionsHandler)
   // r.Methods("GET").Path("/version").Handler(versionHandler)
   r.Methods("GET").Path("/v1/instances").Handler(http.HandlerFunc(s.InstanceList))
+  r.Methods("POST").Path("/v1/instances").Handler(http.HandlerFunc(s.InstanceCreate))
+  r.Methods("DELETE").Path("/v1/instances/{ns}/{name}").Handler(http.HandlerFunc(s.InstanceDelete))
   return r
+}
+
+func (s *server) InstanceDelete(w http.ResponseWriter, r *http.Request) {
+  ns := mux.Vars(r)["ns"]
+  name := mux.Vars(r)["name"]
+
+  err := s.vmClient.VirtualmachineV1alpha1().VirtualMachines(ns).Delete(name, &metav1.DeleteOptions{})
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+    w.Write([]byte(err.Error()))
+  } else {
+    w.WriteHeader(http.StatusNoContent)
+  }
 }
 
 type InstanceList struct {
@@ -70,4 +89,33 @@ func (s *server) InstanceList(w http.ResponseWriter, r *http.Request) {
 
   w.Header().Set("Content-Type", "application/json")
   w.Write(resp)
+}
+
+func (s *server) InstanceCreate(w http.ResponseWriter, r *http.Request) {
+  // TODO get from request params
+  ns := "default"
+  name := "default"
+  cpus := int32(1)
+  mem := int32(512)
+  image := vmapi.MachineImageUbuntu
+  action := vmapi.ActionStart
+
+  vm := &vmapi.VirtualMachine{
+    ObjectMeta: metav1.ObjectMeta{
+      Name:      name,
+    },
+    Spec: vmapi.VirtualMachineSpec{
+      Cpus: cpus,
+      MemoryMB: mem,
+      MachineImage: image,
+      Action: action,
+    },
+  }
+
+  vm, err := s.vmClient.VirtualmachineV1alpha1().VirtualMachines(ns).Create(vm)
+  if err != nil {
+    w.WriteHeader(http.StatusInternalServerError)
+  } else {
+    w.WriteHeader(http.StatusCreated)
+  }
 }
