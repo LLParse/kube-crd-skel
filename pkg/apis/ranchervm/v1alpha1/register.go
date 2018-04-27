cb22fa4bef1eed32590ad90cea134e51548ab0c4
James Oliver <james@rancher.com>
2018-04-27 03:01:49 -0700

    Add RBAC to deployment

--- pkg/apis/ranchervm/v1alpha1/register.go
+++ pkg/apis/ranchervm/v1alpha1/register.go
@@ -4,12 +4,10 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-
-	"github.com/llparse/kube-crd-skel/pkg/apis/ranchervm"
 )
 
 // SchemeGroupVersion is group version used to register these objects
-var SchemeGroupVersion = schema.GroupVersion{Group: ranchervm.GroupName, Version: "v1alpha1"}
+var SchemeGroupVersion = schema.GroupVersion{Group: "vm.rancher.com", Version: "v1alpha1"}
 
 // Kind takes an unqualified kind and returns back a Group qualified GroupKind
 func Kind(kind string) schema.GroupKind {
